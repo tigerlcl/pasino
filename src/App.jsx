8cf13ec85688069a342abc968954ae69dd5c8fc8
Tiger Li <tigerlicuhk@gmail.com>
2024-11-12 15:21:35 +0800

    Refactor App and Footer components; Review CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react";
-import { extendTheme } from '@chakra-ui/react';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Rules from './pages/Rules';
@@ -11,52 +10,10 @@ import Profile from './pages/Profile';
 import FAQ from './pages/FAQ';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './styles/global.css';
-
-// Define the color mode config
-const config = {
-  initialColorMode: 'system',
-  useSystemColorMode: true,
-};
-
-const theme = extendTheme({ 
-  config,
-  styles: {
-    global: (props) => ({
-      body: {
-        bg: props.colorMode === 'dark' ? 'gray.900' : 'gray.50',
-        color: props.colorMode === 'dark' ? 'white' : 'gray.800',
-      },
-    }),
-  },
-  components: {
-    Box: {
-      baseStyle: (props) => ({
-        bg: props.colorMode === 'dark' ? 'gray.800' : 'white',
-      }),
-    },
-    Container: {
-      baseStyle: {
-        maxW: 'container.xl',
-      },
-    },
-    Heading: {
-      baseStyle: (props) => ({
-        color: props.colorMode === 'dark' ? 'white' : 'gray.800',
-      }),
-    },
-    Text: {
-      baseStyle: (props) => ({
-        color: props.colorMode === 'dark' ? 'gray.100' : 'gray.800',
-      }),
-    },
-  },
-});
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
+    <ChakraProvider>
       <Router>
         <Header />
         <Routes>
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/Hero.css';
 import './styles/theme.css';
 import './styles/components.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
